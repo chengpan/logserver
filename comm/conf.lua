d17d91eae15ace4f3f7f6a99a2d3e3a335525493
chengpan <chengpandyx@126.com>
2016-11-23 15:13:07 +0800

    aa

--- comm/conf.lua
+++ comm/conf.lua
@@ -14,6 +14,6 @@ _M.hadoop_db =
 _M.hadoop_root = "/logs/"
 _M.mysql_max_results = 500
 _M.segment_size = 500*1024*1024
-_M.log_download_url = "http://106.75.7.142/log_download.lua"
+_M.log_download_url = "http://106.75.7.142/download_log.lua"
 
 return _M
