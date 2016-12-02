46850c1d327088fb599e276dfd944030367934c2
chengpan <chengpandyx@126.com>
2016-12-02 10:30:09 +0800

    aa

--- get_download_whole_domains.lua
+++ get_download_whole_domains.lua
@@ -1,1 +1,11 @@
-cedce
+local json  = require "cjson.safe"
+local mysql = require "resty.mysql"
+local conf  = require "comm/conf"
+local util  = require "comm/util"
+local webhdfs  = require "comm/webhdfs"
+local shell = require "resty/shell"
+local resty_string = require "resty/string"
+
+for i,v in ipairs(conf.download_whole_domains) do
+	ngx.say(v)
+end
