dc23f7e4e9af6787b4bb265fab5dd23582e032e0
chengpan <chengpandyx@126.com>
2016-12-02 15:21:00 +0800

    aa

--- get_log_urls.lua
+++ get_log_urls.lua
@@ -18,20 +18,27 @@ if res.status ~= 200 then
 	ngx.exit(res.status)
 end
 
+ngx.log(ngx.ERR, "get_log_list: ", json.encode(res))
+
 local log_url_array = json.decode(res.body)
 if #log_url_array == 0 then
-	ngx.print(json.empty_array)
+	ngx.log(ngx.ERR, "no logs found")
+	ngx.print(json.encode(json.empty_array))
 	ngx.exit(ngx.HTTP_OK)
 end
 
 local ret_urls = {}
 for i,v in ipairs(log_url_array) do
-	local query_url = "/get_log_list.lua?id="..v.id
+	local query_url = "/get_download_info.lua?id="..v.id
+	ngx.log(ngx.ERR, "query_url: ", query_url)
+
 	local res = ngx.location.capture(query_url)
 	if not res then
 		ngx.log(ngx.ERR, "no res, ", query_url)
 		ngx.exit(ngx.HTTP_SERVICE_UNAVAILABLE)
 	end
+
+	ngx.log(ngx.ERR, "get_download_info.lua: ", json.encode(res))
 
 	if res.status ~= 200 then
 		ngx.log(ngx.ERR, "res.status: ", res.status, ", query_url: ", query_url)
