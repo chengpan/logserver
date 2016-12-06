0fd2194673f11d2b09ace4194ddedf722a245c2b
chengpan <chengpandyx@126.com>
2016-12-06 20:27:31 +0800

    aa

--- routine_work.sh
+++ routine_work.sh
@@ -51,6 +51,19 @@ for domain in ${domain_names}; do
 			hdfs_file_location="/logs/"${file_date}"/"${domain}"/"${file_type}"_"${file_date_hour}"_access.log"
 			local_file_location=${logs_dir}${file_date}"/"${domain}"/"${file_type}"_"${file_date_hour}"_access.log.gz"
 			
+			check_file_url1="http://10.9.103.2/logs/"${file_date}"/"${domain}"/"${file_type}"_"${file_date_hour}"_access.log.gz"
+			check_file_url2="http://10.9.139.51/logs/"${file_date}"/"${domain}"/"${file_type}"_"${file_date_hour}"_access.log.gz"
+			found=`curl --silent --head ${check_file_url1} | grep 'HTTP' | grep '200'`
+			if [ -n "${found}" ]; then
+				echo "found by ${check_file_url1}" 
+				continue
+			fi
+			found=`curl --silent --head ${check_file_url2} | grep 'HTTP' | grep '200'`
+			if [ -n "${found}" ]; then
+				echo "found by ${check_file_url2}" 
+				continue
+			fi			
+
 			echo "downloading ${hdfs_file_location} to ${local_file_location}"
 
 			if [ -f ${local_file_location} ]; then
@@ -77,15 +90,67 @@ for domain in ${domain_names}; do
 	done
 done
 
-#删除30天以前的日志
-old_log_date=`date -d "-30 days" "+%Y%m%d"`
+success=0
+for domain in ${domain_names}; do
+	if [ $success -lt 1 ]; then
+		break
+	fi
+
+	echo "downloading the whole day log for ${domain} at `date`" | tee -a ${err_log}
+	for file_type in "small" "big"; do
+		file_date=`date -d "-1 day" "+%Y%m%d"`
+		hdfs_file_dir="/logs/"${file_date}"/"${domain}
+		hdfs_file_location="/logs/"${file_date}"/"${domain}"/"${file_type}"*"
+		local_file_location=${logs_dir}${file_date}"/"${domain}"/"${file_type}"_"${file_date}"_access.log.gz"
+
+		check_file_url1="http://10.9.103.2/logs/"${file_date}"/"${domain}"/"${file_type}"_"${file_date}"_access.log.gz"
+		check_file_url2="http://10.9.139.51/logs/"${file_date}"/"${domain}"/"${file_type}"_"${file_date}"_access.log.gz"
+		found=`curl --silent --head ${check_file_url1} | grep 'HTTP' | grep '200'`
+		if [ -n "${found}" ]; then
+			echo "found by ${check_file_url1}" 
+			continue
+		fi
+		found=`curl --silent --head ${check_file_url2} | grep 'HTTP' | grep '200'`
+		if [ -n "${found}" ]; then
+			echo "found by ${check_file_url2}" 
+			continue
+		fi			
+		echo "downloading ${hdfs_file_location} to ${local_file_location}"	
+		
+		if [ -f ${local_file_location} ]; then
+			echo "${local_file_location} has been there"
+			continue
+		fi
+
+		hdfs dfs -test -d ${hdfs_file_dir}
+
+		if [ $? -ne 0 ]; then
+			echo "${hdfs_file_dir} doesnot exist"
+			continue 
+		fi
+
+		mkdir -p `dirname ${local_file_location}`
+
+		hdfs dfs -cat ${hdfs_file_location} | gzip --fast > ${local_file_location}
+		if [ $? -ne 0 ]; then
+			echo "download ${hdfs_file_location} failed" | tee -a ${err_log}
+			rm -f ${local_file_location}
+			continue
+		fi
+
+		let success++
+	done
+done
+
+#删除10天以前的日志
+old_log_date=`date -d "-10 days" "+%Y%m%d"`
 old_log_dir=${logs_dir}${old_log_date}
 
 echo "removing ${old_log_dir}"
 rm -rf ${old_log_dir}
 
 #删除10天未访问的日志
-find ${logs_dir} -type f -atime +10 -delete
+#find ${logs_dir} -type f -atime +10 -delete
 
 #进程结束
 rm -f ${process_pid}
